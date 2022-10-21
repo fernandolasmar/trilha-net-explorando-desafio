f397502341cb5e9ef08bfacc78202d6c5f8b3a6f
Fernando Lasmar <f3rnandolasmar@gmail.com>
2022-10-21 19:16:07 -0300

    Desafio sistema de hospedagem de um hotel - reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -14,17 +14,17 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+        {            
+            var totalDeHospedes = hospedes.Count;            
+            
+            if (Suite.Capacidade >= totalDeHospedes)
             {
                 Hospedes = hospedes;
             }
             else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+            {                
+                Console.WriteLine($"Capacidade máxima da suíte: {Suite.Capacidade}, quantidade de hóspedes: {totalDeHospedes}, " +
+                                    "você excedeu a capacidade da suíte.");
             }
         }
 
@@ -34,24 +34,21 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+        {            
+            var totalDeHospedes = Hospedes.Count;
+            return totalDeHospedes;
         }
 
         public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+        {            
+            var totalDaDiaria = DiasReservados * Suite.ValorDiaria;
+            decimal valor = totalDaDiaria;
+            
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                var descontoDiaria = (totalDaDiaria * 10) / 100;                
+                valor = totalDaDiaria - descontoDiaria;
+                Console.WriteLine($"Valor total: {totalDaDiaria}, você recebeu um desconto de {descontoDiaria}");
             }
 
             return valor;
